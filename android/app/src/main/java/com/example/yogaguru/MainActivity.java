90fdbb6b05ba5baf2aa6046c1793aafcaa7f10a0
Aditya <adityaas26@gmail.com>
2019-11-04 00:29:19 +0530

    Remove unwanted comments

--- android/app/src/main/java/com/example/yogaguru/MainActivity.java
+++ android/app/src/main/java/com/example/yogaguru/MainActivity.java
@@ -29,13 +29,6 @@ public class MainActivity extends FlutterActivity {
 		super.onCreate(savedInstanceState);
 		GeneratedPluginRegistrant.registerWith(this);
 
-		// try {
-		// tflite = new Interpreter(loadModelFile());
-		// } catch (Exception e) {
-		// System.out.println("Exception while loading: " + e);
-		// throw new RuntimeException(e);
-		// }
-
 		new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(new MethodCallHandler() {
 			@Override
 			public void onMethodCall(MethodCall call, Result result) {
@@ -69,25 +62,15 @@ public class MainActivity extends FlutterActivity {
 			inputArray[0][i] = e.floatValue();
 			i++;
 		}
-		// System.out.println("Array: " + input_data);
 		float[][] output_data = new float[1][1];
 		tflite.run(inputArray, output_data);
-		// for (int j = 0; j < 3; j++)
+
 		Log.d("tag", ">> " + output_data[0][0]);
-		// if (output_data[0][0] > 0.5) {
-		// return "0";
-		// } else if (output_data[0][1] > 0.5) {
-		// return "1";
-		// } else if (output_data[0][2] > 0.5) {
-		// return "2";
-		// } else {
-		// return "Wrong Pose";
-		// }
+
 		return output_data[0][0];
 	}
 
 	// method to load tflite file from device
-
 	private MappedByteBuffer loadModelFile(String modelName) throws Exception {
 		AssetFileDescriptor fileDescriptor = this.getAssets().openFd(modelName);
 		FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
